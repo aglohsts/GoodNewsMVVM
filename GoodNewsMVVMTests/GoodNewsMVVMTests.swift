998015ff04f8b88a6e55ec8459e8dc41cedb0160
Agnes <agneslo@koodata.com.tw>
2019-12-17 16:51:44 +0800

    Initial Commit

---
+++ GoodNewsMVVMTests/GoodNewsMVVMTests.swift
@@ -0,0 +1,34 @@
+//
+//  GoodNewsMVVMTests.swift
+//  GoodNewsMVVMTests
+//
+//  Created by Agnes Lo on 2019/12/17.
+//  Copyright © 2019 Agnes Lo. All rights reserved.
+//
+
+import XCTest
+@testable import GoodNewsMVVM
+
+class GoodNewsMVVMTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
