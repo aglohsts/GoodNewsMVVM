07dd51354c04036b5a4878ef64e3e82db882f061
Agnes <agneslo@koodata.com.tw>
2019-12-23 17:49:57 +0800

    Lay out NewsListTableViewController.

--- GoodNewsMVVM/Controllers/NewsListTableViewController.swift
+++ GoodNewsMVVM/Controllers/NewsListTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class NewsListTableViewController: UITableViewController {
+    
+    private var articleListVM: ArticleListViewModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,74 +24,42 @@ class NewsListTableViewController: UITableViewController {
         
         let url = URL(string: "https://newsapi.org/v2/everything?q=bitcoin&from=2019-11-23&sortBy=publishedAt&apiKey=a71e711b76ed49f1b36a4445908129fb")!
         
-        WebService().getArticle(url: url, completion: { (_) in
+        WebService().getArticle(url: url, completion: { [weak self] articles in
             
+            guard let articles = articles else { return }
+            
+            self?.articleListVM = ArticleListViewModel(articles: articles)
+            
+            DispatchQueue.main.async {
+                
+                self?.tableView.reloadData()
+            }
         })
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return self.articleListVM == nil ? 0 : self.articleListVM!.numberOfSections
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.articleListVM == nil ? 0 : self.articleListVM!.numberOfRowsInSection(section)
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ArticleTableViewCell.self), for: indexPath) as? ArticleTableViewCell else {
+            
+            fatalError("ArticleTableViewCell not found")
+        }
+        
+        let articleVM = self.articleListVM?.articleAtIndex(indexPath.row)
+        
+        cell.titleLabel.text = articleVM?.title
+        cell.descriptionLabel.text = articleVM?.description
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
